160e47704d0346136e177423671dd9f37eb8640f
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2022-06-05 13:09:29 +0200

    test

--- test/drag-and-drop-v2/script.js
+++ test/drag-and-drop-v2/script.js
@@ -31,7 +31,6 @@ function onDrop(event) {
     event
         .dataTransfer
         .clearData();
-    draggableElement.removeAttribute("draggable")
-    draggableElement.removeAttribute("ondragstart")
+    
     console.log(event);
 }
